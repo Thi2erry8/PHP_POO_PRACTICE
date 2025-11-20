6656ac3ccccecbb3910e3141144ba843271f0960
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-11-19 23:23:25 -0500

    My_First Class

---
+++ inc/NewClass.php
@@ -0,0 +1,10 @@
+<?php 
+    class NewClass {
+        //Properties and methods goes here
+        public $info = "This is a new class!";
+    }
+
+    $object = new NewClass;
+    var_dump($object);
+
+?>
