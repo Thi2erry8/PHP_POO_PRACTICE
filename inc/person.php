f6c68a527ba58596176af94f1148eb476783710f
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-11-22 16:46:57 -0500

    #7

--- inc/person.php
+++ inc/person.php
@@ -5,6 +5,6 @@
         public $age;
 
         public function greet(){
-            return "Hello my name is " . $this->name . " and I am " . $this->age . " years old.";
+            return "Hello  my name is " . $this->name . " and I am " . $this->age . " years old.";
         }
     }
