4df4ed4191efbafb266bdf4c44c95500eb74e10b
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-11-25 18:28:59 -0500

    autoload

--- index.php
+++ index.php
@@ -11,7 +11,18 @@
 </head>
 
 <body>
-
+    <form action="inc/calc.php" method="post">
+        <p>My own calculator</p>
+        <input type="number" name="num1" placeholder="First number">
+        <select name="oper">
+            <option value="add">Addiction</option>
+            <option value="sub">Subtraction</option>
+            <option value="div">Division</option>
+            <option value="mul">Multiplication</option>
+        </select>
+        <input type="number" name="num2" placeholder="Second numder">
+        <button type="submit" name="submit">Calculatrice</button>
+    </form>
 </body>
 
 </html>
