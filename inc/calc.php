205065f314306c63d4d53537c8edd3a3f642a0d4
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-11-25 22:36:46 -0500

    #13

--- inc/calc.php
+++ inc/calc.php
@@ -1,6 +1,38 @@
 <?php
      
      class Calc {
-                 
+                 public $num1;
+                 public $num2;
+                 public $oper;
+
+        public function __construct(string $one , int $two , int $three){
+            $this->num1 = $two;
+            $this->num2 = $three;
+            $this->oper = $one;
+        }
+
+        
+        public function calculator(){
+            switch($this->oper){
+                case "add":
+                    return $this->num1 + $this->num2;
+                    break;
+                case "sub":
+                    return $this->num1 - $this->num2;
+                    break;
+                case "div":
+                    if($this->num2 == 0){
+                        return "Division by zero is not allowed.";
+                    } else {
+                        return $this->num1 / $this->num2;
+                    }
+                    break;
+                case "mul":
+                    return $this->num1 * $this->num2;
+                    break;
+                default:
+                    return "Invalid operation.";    
+            }
+        }
      }
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-      declare(strict_types=1)
+      declare(strict_types=1);
+      include 'inc/autoload.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
